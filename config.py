9ff0bf6b70fa2445200f7ac11f91c004d12332b2
efraimzz <85498030+efraimzz@users.noreply.github.com>
2026-03-04 02:48:55 +0200

    Fix formatting of REQUEST_GROUP URL

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ ADMINS = [int(x) for x in os.environ.get("ADMINS", "").replace(",", " ").split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
 UPDATE_CHANNEL = "searchgram_bots"
-REQUEST_GROUP = "https://t.me/searchgram_group
+REQUEST_GROUP = "https://t.me/searchgram_group"
 
 PHOTO_URL = "https://i.ibb.co/BK2j0c7p/x.jpg"
 
